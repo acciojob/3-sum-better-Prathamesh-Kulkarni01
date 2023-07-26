ee9ac80f0eb18b0779a52e8151fe81ad84ec3479
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-07-26 19:46:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let i=0;
+	arr=arr.sort()
+	let closest=9999
+	while(arr.length-2>i){
+		let j=i+1;
+		let k=arr.length-1;
+		while(j<k){
+			let sum=arr[i]+arr[j]+arr[k]
+			if(Math.abs(target-sum)<Math.abs(target-closest)){
+				closest=sum
+			}
+			if(sum>target){
+				k--;
+			}else{
+				j++
+			}
+			
+		}
+		i++;
+	}
+  return closest
 }
 
 module.exports = threeSum;
